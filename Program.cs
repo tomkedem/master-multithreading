4613b03c1396eb294209a2b3fe2c7a0652438381
Tomer Kedem <topha1@hotmail.com>
2025-03-17 06:51:02 +0200

    Exclusive Lock and Atomic Operation

--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
 ﻿int counter = 0;
+object lockObject = new object();
 Thread t1 = new Thread(IncrementCounter);
-t1.Start();
-t1.Join();
-
 Thread t2 = new Thread(IncrementCounter);
 t2.Start();
+t1.Start();
 t2.Join();
+t1.Join();
 Console.WriteLine($"Final counter value is:{counter}");
 
 void IncrementCounter()
 {
     for (int i = 0; i < 100000; i++)
     {
-        counter++;
+        lock (lockObject)
+        {
+            counter++;
+        }
     }   
 }
